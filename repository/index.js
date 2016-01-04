be9bd0c588484c910310269f8d95cbaad945847d
GKB <guankaobo@suishipen.com>
2016-01-04 15:36:15 +0800

    去除repository code返回，整理仓库内容，@1.4.9

--- repository/index.js
+++ repository/index.js
@@ -12,17 +12,17 @@ mysql.query = function(sql, args, callback) {
         this.pool.getConnection(function(err, connection) {
             if (err) {
                 console.log(err);
-                callback(err, 2001, null);
+                callback(err, null);
                 return;
             }
             connection.query(sql, args, function(err, results) {
                 connection.release();
                 if (err) {
-                    callback(err, 2000, null);
+                    callback(err, null);
                     return;
                 }
 
-                callback(false, 200, results);
+                callback(false, results);
             });
         });
     } else {
@@ -31,111 +31,57 @@ mysql.query = function(sql, args, callback) {
 
 };
 
-mysql.find = function(sql, args, callback) {
-    this.query(sql, args, function(err, code, results) {
-        if (err) return callback(err, code === 2000 ? 2002 : code, results);
-        callback(err, code, results);
-    });
-};
-mysql.insert = function(sql, args, callback) {
-    this.query(sql, args, function(err, code, results) {
-        if (err) return callback(err, code === 2000 ? 2004 : code, results);
-        callback(err, code, results);
-    });
-};
-mysql.update = function(sql, args, callback) {
-    this.query(sql, args, function(err, code, results) {
-        if (err) return callback(err, code === 2000 ? 2006 : code, results);
-        callback(err, code, results);
-    });
-};
-mysql.delete = function(sql, args, callback) {
-    this.query(sql, args, function(err, code, results) {
-        if (err) return callback(err, code === 2000 ? 2007 : code, results);
-        callback(err, code, results);
-    });
-};
-
 mysql.begin = function(callback) {
     if (this.pool) {
         this.pool.getConnection(function(err, connection) {
             if (err) {
-                callback(err, 2001, null);
+                callback(err, null);
                 return;
             }
             connection.beginTransaction(function(err) {
                 if (err) {
-                    callback(err, 2005, null);
+                    callback(err, null);
                     return;
                 }
 
                 var trans = function(connection) {
-                    var _conn = connection;
 
                     return {
                         commit: function(callback) {
-                            _conn.commit(function(err, results) {
+                            connection.commit(function(err, results) {
                                 if (err) {
-                                    return _conn.rollback(function(err, results) {
+                                    return connection.rollback(function(err, results) {
                                         connection.release();
-                                        callback(err, 2005, null);
-                                    });
-                                }
-                                connection.release();
-                                callback(false, 200, results);
-                            });
-                        },
-                        rollback: function(callback) {
-                            _conn.rollback(function(err, results) {
-                                connection.release();
-                                callback(err, 2005, null);
-                            });
-                        },
-                        inject: function(model) {
-                            model._connection = connection;
-                            return model;
-                        },
-                        query: function(sql, args, callback) {
-                            var self = this;
-                            _conn.query(sql, args, function(err, results) {
-                                if (err) {
-                                    console.log(err);
-                                    return self.rollback(function(e, c, r) {
-                                        callback(err, 2000, null);
+                                        callback(err, null);
                                     });
                                 }
 
-                                callback(null, 200, results);
+                                connection.release();
+                                callback(null, results);
                             });
                         },
-                        find: function(sql, args, callback) {
-                            this.query(sql, args, function(err, code, results) {
-                                if (err) return callback(err, code === 2000 ? 2002 : code, results);
-                                callback(err, code, results);
+                        rollback: function(callback) {
+                            connection.rollback(function(err, results) {
+                                connection.release();
+                                callback(err, null);
                             });
                         },
-                        insert: function(sql, args, callback) {
-                            this.query(sql, args, function(err, code, results) {
-                                if (err) return callback(err, code === 2000 ? 2004 : code, results);
-                                callback(err, code, results);
-                            });
-                        },
-                        update: function(sql, args, callback) {
-                            this.query(sql, args, function(err, code, results) {
-                                if (err) return callback(err, code === 2000 ? 2006 : code, results);
-                                callback(err, code, results);
-                            });
-                        },
-                        delete: function(sql, args, callback) {
-                            this.query(sql, args, function(err, code, results) {
-                                if (err) return callback(err, code === 2000 ? 2007 : code, results);
-                                callback(err, code, results);
+                        query: function(sql, args, callback) {
+                            connection.query(sql, args, function(err, results) {
+                                if (err) {
+                                    return connection.rollback(function(err, results) {
+                                        connection.release();
+                                        callback(err, null);
+                                    });
+                                }
+
+                                callback(null, results);
                             });
                         }
                     };
                 };
 
-                callback(null, 200, trans(connection));
+                callback(null, trans(connection));
             });
         });
     } else {
